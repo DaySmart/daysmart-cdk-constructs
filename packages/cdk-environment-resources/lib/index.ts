6b99b0e908e7447c20150d0014534e51ad5168b8
Matthew Arteaga <matthew.arteaga@daysmart.com>
2021-05-17 15:09:59 -0400

    PLATFORM-1 Removed OriginAccessIdentity and Bucket Policy from this construct

--- packages/cdk-environment-resources/lib/index.ts
+++ packages/cdk-environment-resources/lib/index.ts
@@ -29,33 +29,11 @@ export class CdkEnvironmentResources extends cdk.Construct {
             versioned: true,
         });
 
-        const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'OriginAcessIdentity', {
-            comment: `OriginAccessIdentity for ${bucket.bucketName}.`
-        });
-
-        const bucketPolicy = new s3.BucketPolicy(this, 'BucketPolicy', {
-            bucket: bucket
-        });
-
-        bucketPolicy.document.addStatements(
-            new iam.PolicyStatement({
-                effect: iam.Effect.ALLOW,
-                principals: [originAccessIdentity.grantPrincipal],
-                actions: ['s3:GetObject'],
-                resources: [bucket.bucketArn + "/*"],
-            })
-        );
-
         let output = new cdk.CfnOutput(this, "AppspecBucket", {
             value: bucket.bucketName
         });
 
-        let output2 = new cdk.CfnOutput(this, "OriginAccessIdentity", {
-            value: originAccessIdentity.originAccessIdentityName
-        });
-
         output.overrideLogicalId("AppspecBucket");
-        output2.overrideLogicalId("OriginAccessIdentity");
 
         const cluster = new ecs.Cluster(this, "Cluster", {
             clusterName: `${props.stage}-${props.project}`,
