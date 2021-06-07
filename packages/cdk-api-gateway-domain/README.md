667527aac41d67d90eea00aecca9912c531bb86c
Matthew Arteaga <matthew.arteaga@daysmart.com>
2021-06-07 13:59:09 -0400

    PLATFORM-1 Updated the cdk code to use escape hatches rather than custom resources, as well as support dynamic env domain naming.  I also updated the readme to be more descriptive in the purpose of the construct and the input definitions.

--- packages/cdk-api-gateway-domain/README.md
+++ packages/cdk-api-gateway-domain/README.md
@@ -1,12 +1,20 @@
-# Welcome to your CDK TypeScript Construct Library project!
+# Welcome to the Open Source Construct for an Api Gateway Custom Domain!
 
-You should explore the contents of this project. It demonstrates a CDK Construct Library that includes a construct (`CdkApiGatewayDomain`)
-which contains an Amazon SQS queue that is subscribed to an Amazon SNS topic.
+This CDK Construct Library includes a construct (`CdkApiGatewayDomain`)
+which creates a custom domain for the specified API Gateway api, along with a base path mapping and route53 alias record to the endpoint cloudfront distribution
 
-The construct defines an interface (`CdkApiGatewayDomainProps`) to configure the visibility timeout of the queue.
+The construct defines an interface (`CdkApiGatewayDomainProps`) with the following properties that can be passed in:
+
+  * companyDomainName => The Hosted Zone name in Route53 (ex. daysmart.com)
+  * companyHostedZoneId => The Hosted Zone ID in Route53 (ex. "Z2FDTNDATAQYW2")
+  * project => The name of the project that is using this construct for some of its resources. (ex. pdfconverter)
+  * baseEnv => The name of your base environment. (ex. dev, stage, prod)
+  * dynamicEnv? => The name of a dynamic environment if applicable. (ex. matt-test)
+  * certificateArn => The arn of a valid AWS Certificate Manager certificate that covers your intended custom domain name.  This can be a raw string, but should preferably be a Frank reference to the cert. arn output of another CloudFormation stack, made by the @cdk-certificate module. (ex. ${matttest:pdfconverter-cert-06-04-2021:CertificateArn} => The first part of a Frank reference is the env specified in the Frank template for the certificate stack; the second portion is the certificate stack name; the last part is the name of the desired stack output which should almost always be "CertificateArn")
+  * restApiId => The ID of an API Gateway Rest API, provisioned beforehand through serverless or some other provider (ex. ae2u06ed94)
+  * basePath => The desired path for the api's base path mapping (ex. api)
 
 ## Useful commands
 
  * `npm run build`   compile typescript to js
  * `npm run watch`   watch for changes and compile
- * `npm run test`    perform the jest unit tests
--- packages/cdk-api-gateway-domain/lib/index.ts
+++ packages/cdk-api-gateway-domain/lib/index.ts
@@ -1,16 +1,14 @@
 import * as cdk from '@aws-cdk/core';
 import * as apigw from '@aws-cdk/aws-apigateway';
 import * as acm from '@aws-cdk/aws-certificatemanager';
-import * as iam from '@aws-cdk/aws-iam';
 import * as route53 from '@aws-cdk/aws-route53';
-import * as targets from '@aws-cdk/aws-route53-targets';
-import * as customresource from '@aws-cdk/custom-resources'
 
 export interface CdkApiGatewayDomainProps {
   companyDomainName: string;
   companyHostedZoneId: string;
   project: string;
-  stage: string;
+  baseEnv: string;
+  dynamicEnv?: string;
   certificateArn: string;
   restApiId: string;
   basePath: string;
@@ -23,144 +21,41 @@ export class CdkApiGatewayDomain extends cdk.Construct {
 
     const api = apigw.RestApi.fromRestApiId(this, "Rest API", `${props.restApiId}`);
 
-    const customDomain = new apigw.DomainName(this, 'Custom Domain', {
-      domainName: (props.stage == "prod") ? `api.${props.project}.${props.companyDomainName}` : `api.${props.stage}.${props.project}.${props.companyDomainName}`,
-      certificate: acm.Certificate.fromCertificateArn(this, "Certificate", `${props.certificateArn}`),
-      endpointType: apigw.EndpointType.EDGE,
-      securityPolicy: apigw.SecurityPolicy.TLS_1_0
+    let customDomain: apigw.DomainName;
+
+    if (props.dynamicEnv) {
+      customDomain = new apigw.DomainName(this, 'Custom Domain', {
+        domainName: `${props.dynamicEnv}-api.${props.baseEnv}.${props.project}.${props.companyDomainName}`,
+        certificate: acm.Certificate.fromCertificateArn(this, "Certificate", `${props.certificateArn}`),
+        endpointType: apigw.EndpointType.EDGE,
+        securityPolicy: apigw.SecurityPolicy.TLS_1_0
+      });
+    } else {
+      customDomain = new apigw.DomainName(this, 'Custom Domain', {
+        domainName: (props.baseEnv == "prod") ? `api.${props.project}.${props.companyDomainName}` : `api.${props.baseEnv}.${props.project}.${props.companyDomainName}`,
+        certificate: acm.Certificate.fromCertificateArn(this, "Certificate", `${props.certificateArn}`),
+        endpointType: apigw.EndpointType.EDGE,
+        securityPolicy: apigw.SecurityPolicy.TLS_1_0
+      });
+    }
+
+    const cloudformationBasePathMapping = new apigw.CfnBasePathMapping(this, "CloudformationBasePathMapping", {
+      basePath: `${props.basePath}`,
+      domainName: customDomain.domainName,
+      restApiId: `${props.restApiId}`,
+      stage: `${props.baseEnv}` 
     });
 
-    let apiGatewayCreateBasePathMappingCall: customresource.AwsSdkCall = {
-      service: 'APIGateway',
-      action: 'createBasePathMapping',
-      parameters: {
-        domainName: customDomain.domainName,
-        restApiId: `${props.restApiId}`,
-        basePath: `${props.basePath}`,
-        stage: `${props.stage}`
+    const cloudformationRoute53Change = new route53.CfnRecordSet(this, "CloudformationAliasRecordRoute53", {
+      aliasTarget: {
+        dnsName: customDomain.domainNameAliasDomainName,
+        evaluateTargetHealth: false,
+        hostedZoneId: "Z2FDTNDATAQYW2"
       },
-      physicalResourceId: customresource.PhysicalResourceId.of('base-path-mapping-custom-resource')
-    }
-
-    const apiGatewayBasePathMapping = new customresource.AwsCustomResource(this, 'ApiGatewayBasePathMapping', {
-      onCreate: apiGatewayCreateBasePathMappingCall,
-      onDelete: {
-        service: 'APIGateway',
-        action: 'deleteBasePathMapping',
-        parameters: {
-          basePath: `${props.basePath}`,
-          domainName: customDomain.domainName
-        }
-      },
-      policy: customresource.AwsCustomResourcePolicy.fromStatements([
-        new iam.PolicyStatement({
-          actions: [
-            "apigateway:*"
-          ],
-          resources: [
-            `arn:aws:apigateway:${cdk.Stack.of(this).region}::/restapis/${api.restApiId}/*`,
-            `arn:aws:apigateway:${cdk.Stack.of(this).region}::/domainnames/${customDomain.domainName}/*`
-          ],
-          effect: iam.Effect.ALLOW
-        })
-      ])
-    })
-
-    let route53ChangeResourceRecordSetsCall: customresource.AwsSdkCall = {
-      service: 'Route53',
-      action: 'changeResourceRecordSets',
-      parameters: {
-        ChangeBatch: {
-          Changes: [
-            {
-              Action: "UPSERT",
-              ResourceRecordSet: {
-                AliasTarget: {
-                  DNSName: customDomain.domainNameAliasDomainName,
-                  EvaluateTargetHealth: false,
-                  HostedZoneId: "Z2FDTNDATAQYW2"
-                },
-                Name: customDomain.domainName,
-                Type: "A"
-              }
-            }
-          ],
-          Comment: `CloudFront distribution for ${customDomain.domainName}`
-        },
-        HostedZoneId: props.companyHostedZoneId
-      },
-      physicalResourceId: customresource.PhysicalResourceId.of('cloudfront-alias-record-custom-resource')
-    }
-
-    const route53CloudfrontCustomDomainAlias = new customresource.AwsCustomResource(this, 'Route53CloudfrontCustomDomainAlias', {
-      onCreate: route53ChangeResourceRecordSetsCall,
-      onDelete: {
-        service: 'Route53',
-        action: 'changeResourceRecordSets',
-        parameters: {
-          ChangeBatch: {
-            Changes: [
-              {
-                Action: "DELETE",
-                ResourceRecordSet: {
-                  AliasTarget: {
-                    DNSName: customDomain.domainNameAliasDomainName,
-                    EvaluateTargetHealth: false,
-                    HostedZoneId: "Z2FDTNDATAQYW2"
-                  },
-                  Name: customDomain.domainName,
-                  Type: "A"
-                }
-              }
-            ],
-            Comment: `CloudFront distribution for ${customDomain.domainName}`
-          },
-          HostedZoneId: props.companyHostedZoneId
-        }
-      },
-      policy: customresource.AwsCustomResourcePolicy.fromStatements([
-        new iam.PolicyStatement({
-          actions: [
-            "apigateway:*"
-          ],
-          resources: [
-            `arn:aws:apigateway:${cdk.Stack.of(this).region}::/restapis/${api.restApiId}/*`,
-            `arn:aws:apigateway:${cdk.Stack.of(this).region}::/domainnames/${customDomain.domainName}/*`
-          ],
-          effect: iam.Effect.ALLOW
-        }),
-        new iam.PolicyStatement({
-          actions: [
-            "cloudfront:*"
-          ],
-          resources: [
-            `arn:aws:cloudfront::${cdk.Stack.of(this).account}:distribution/*`
-          ],
-          effect: iam.Effect.ALLOW
-        }),
-        new iam.PolicyStatement({
-          actions: [
-            "route53:*"
-          ],
-          resources: [
-            `arn:aws:route53:::hostedzone/Z2FDTNDATAQYW2`,
-            `arn:aws:route53:::hostedzone/${props.companyHostedZoneId}`,
-            `arn:aws:route53:::*`
-          ],
-          effect: iam.Effect.ALLOW
-        }),
-      ])
-    })
-
-
-    // This was having issues creating a Route53 alias.  However, the custom resource way works fine.
-
-    // const dnsRecord = new route53.ARecord(this, 'CustomDomainAliasRecord', {
-    //   zone: route53.HostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
-    //     hostedZoneId: props.companyHostedZoneId,
-    //     zoneName: props.companyDomainName
-    //   }),
-    //   target: route53.RecordTarget.fromAlias(new targets.ApiGatewayDomain(customDomain))
-    // });
+      comment: `CloudFront distribution for ${customDomain.domainName}`,
+      hostedZoneId: props.companyHostedZoneId,
+      name: customDomain.domainName,
+      type: "A"
+    });
   }
 }
