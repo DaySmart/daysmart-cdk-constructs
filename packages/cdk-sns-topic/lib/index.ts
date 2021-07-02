298e65078a537ed9dc8cb188f95f40f0699d93cb
Matthew Arteaga <matthew.arteaga@daysmart.com>
2021-07-02 10:47:13 -0400

    PLATFORM-1 Removed subscription functionality from construct

--- packages/cdk-sns-topic/lib/index.ts
+++ packages/cdk-sns-topic/lib/index.ts
@@ -1,14 +1,9 @@
 import * as cdk from '@aws-cdk/core';
 import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as lambda from '@aws-cdk/aws-lambda';
 
 export interface CdkSnsTopicProps {
   stage: string;
   project: string;
-  subscription: string;
-  lambdaArn?: string;
-  email?: string;
 }
 
 export class CdkSnsTopic extends cdk.Construct {
@@ -19,15 +14,6 @@ export class CdkSnsTopic extends cdk.Construct {
       displayName: `${props.stage}-${props.project}`,
       topicName: `${props.stage}-${props.project}`
     });
-    let subscription: subs.EmailSubscription | subs.LambdaSubscription;
-
-    if (props.subscription == "lambda" && props.lambdaArn) {
-      subscription = new subs.LambdaSubscription(lambda.Function.fromFunctionArn(this, "LambdaFunction", props.lambdaArn));
-      snsTopic.addSubscription(subscription);
-    } else if (props.subscription == "email" && props.email) {
-      subscription = new subs.EmailSubscription(props.email);
-      snsTopic.addSubscription(subscription);
-    } else { /*Unsupported subscription type*/ }
 
     const topicOutput = new cdk.CfnOutput(this, "SnsTopicArn", {
       value: snsTopic.topicArn
