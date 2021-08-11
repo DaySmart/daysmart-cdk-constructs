871a392dc6ee62842b7c6fe13b61e71f8482d1c1
Matthew Arteaga <matthew.arteaga@daysmart.com>
2021-08-11 12:34:28 -0400

    PLATFORM-1 Removed unnecessary code and paramaterized the cname property

--- packages/cdk-s3-redirect/lib/index.ts
+++ packages/cdk-s3-redirect/lib/index.ts
@@ -16,25 +16,10 @@ export interface CdkS3RedirectProps {
   componentName: string;
   loggingBucketName?: string;
   errorResponses?: cloudfront.ErrorResponse[]
-  oldDomains: string[];
-}
-
-export interface CdkCloudfrontAddHttpOriginProps {
-  project: string;
-  baseEnv: string;
-  origins: HttpOrigin[];
-}
-
-export interface HttpOrigin {
-  host: string;
-  path: string;
+  cname: string;
 }
 
 export class CdkS3Redirect extends cdk.Construct {
-  readonly httpOriginCachePolicy: cloudfront.ICachePolicy;
-  readonly httpOriginRequestPolicy: cloudfront.IOriginRequestPolicy;
-  private distribution: cloudfront.Distribution;
-
   constructor(scope: cdk.Construct, id: string, props: CdkS3RedirectProps) {
     super(scope, id);
 
@@ -54,8 +39,8 @@ export class CdkS3Redirect extends cdk.Construct {
 
     const certificate = acm.Certificate.fromCertificateArn(this, "Certificate", props.certificateArn);
 
-    this.httpOriginCachePolicy = cloudfront.CachePolicy.fromCachePolicyId(this, "HttpOriginCachePolicy", props.httpOriginCachePolicyId);
-    this.httpOriginRequestPolicy = cloudfront.OriginRequestPolicy.fromOriginRequestPolicyId(this, "HttpOriginRequestPolicy", props.httpOriginRequestPolicyId);
+    const httpOriginCachePolicy = cloudfront.CachePolicy.fromCachePolicyId(this, "HttpOriginCachePolicy", props.httpOriginCachePolicyId);
+    const httpOriginRequestPolicy = cloudfront.OriginRequestPolicy.fromOriginRequestPolicyId(this, "HttpOriginRequestPolicy", props.httpOriginRequestPolicyId);
 
     const defaultBehaviorOptions = {
       origin: new origins.HttpOrigin(bucket.bucketWebsiteDomainName, {
@@ -68,18 +53,20 @@ export class CdkS3Redirect extends cdk.Construct {
       }),
       allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
       viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-      cachePolicy: this.httpOriginCachePolicy,
-      originRequestPolicy: this.httpOriginRequestPolicy
+      cachePolicy: httpOriginCachePolicy,
+      originRequestPolicy: httpOriginRequestPolicy
     }
 
     if (props.loggingBucketName) {
       logFilePrefix = (props.dynamicEnv) ? `${props.dynamicEnv}-${props.project}-redirect` : `${props.baseEnv}-${props.project}-redirect`
     }
 
-    this.distribution = new cloudfront.Distribution(this, "Distribution", {
+    const distribution = new cloudfront.Distribution(this, "Distribution", {
       defaultBehavior: defaultBehaviorOptions,
       certificate: certificate,
-      domainNames: this.getAliases(props, props.oldDomains),
+      domainNames: [
+        props.cname
+      ],
       defaultRootObject: "index.html",
       comment: (props.dynamicEnv) ? `${props.dynamicEnv} ${props.project} redirect` : `${props.baseEnv} ${props.project} redirect`,
       enableLogging: (props.loggingBucketName) ? true : false,
@@ -92,45 +79,5 @@ export class CdkS3Redirect extends cdk.Construct {
       minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_1_2016
     });
 
-  }
-
-  getAliases(props: CdkS3RedirectProps, oldDomains: string[]): string[] {
-    let aliases: string[] = [];
-    oldDomains.forEach(oldDomain => {
-      aliases.push(
-        // (props.dynamicEnv) ? `${props.dynamicEnv}-${props.componentName}.${props.baseEnv}.${props.project}.${oldDomain}` : `${props.componentName}.${props.baseEnv}.${props.project}.${oldDomain}`,
-        (props.dynamicEnv) ? `${props.dynamicEnv}-${props.project}.${oldDomain}` : `${props.baseEnv}-${props.project}.${oldDomain}`
-      );
-
-      if (props.dynamicEnv == undefined && props.baseEnv == "prod") {
-        aliases.push(
-          `${props.project}.${oldDomain}`
-        );
-      }
-    });
-
-    return aliases;
-  }
-
-  addHttpOriginBehavior(props: CdkCloudfrontAddHttpOriginProps): void {
-    const httpOrigins = props.origins;
-
-    httpOrigins.forEach(origin => {
-      this.distribution.addBehavior(origin.path, new origins.HttpOrigin(origin.host, {
-        protocolPolicy: cloudfront.OriginProtocolPolicy.HTTPS_ONLY,
-        originSslProtocols: [
-          cloudfront.OriginSslPolicy.TLS_V1_1,
-          cloudfront.OriginSslPolicy.TLS_V1_2
-        ],
-        readTimeout: cdk.Duration.seconds(60)
-      }),
-        {
-          allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
-          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-          cachePolicy: this.httpOriginCachePolicy,
-          originRequestPolicy: this.httpOriginRequestPolicy
-        }
-      );
-    });
   }
 }
