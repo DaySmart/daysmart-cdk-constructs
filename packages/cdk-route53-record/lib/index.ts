6d848e0a5361683a3129bf7e97cd43819bdcab41
Matthew Arteaga <matthew.arteaga@daysmart.com>
2021-12-02 12:16:50 -0500

    new route53 construct for multiple use cases

---
+++ packages/cdk-route53-record/lib/index.ts
@@ -0,0 +1,54 @@
+import * as cdk from "@aws-cdk/core";
+import * as route53 from "@aws-cdk/aws-route53";
+import * as targets from "@aws-cdk/aws-route53-targets";
+import * as elbv2 from "@aws-cdk/aws-elasticloadbalancingv2";
+
+export interface CdkRoute53RecordProps {
+  targetType: string;
+  loadBalancerArn: string;
+  hostedZoneDomainNames: string[];
+  dnsRecords: string[];
+}
+
+export class CdkRoute53Record extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props: CdkRoute53RecordProps) {
+    super(scope, id);
+
+    if (props.dnsRecords.length == props.hostedZoneDomainNames.length) {
+      switch (props.targetType) {
+        case "alb":
+          var loadBalancer = elbv2.ApplicationLoadBalancer.fromLookup(this, "LoadBalancer", {
+            loadBalancerArn: props.loadBalancerArn
+          });
+
+          var albTarget = new targets.LoadBalancerTarget(loadBalancer);
+
+          for (let index = 0; index < props.dnsRecords.length; index++) {
+            const domainHostedZone = route53.HostedZone.fromLookup(this, `${props.hostedZoneDomainNames[index]}-HostedZone`, {
+              domainName: props.hostedZoneDomainNames[index],
+              privateZone: false
+            });
+
+            const dnsRecord = new route53.ARecord(this, `${props.dnsRecords[index]}-Record`, {
+              target: route53.RecordTarget.fromAlias(albTarget),
+              zone: domainHostedZone,
+              recordName: props.dnsRecords[index]
+            });
+          }
+          break;
+        // NOT YET IMPLEMENTED
+        // case "nlb":
+
+        //   break;
+        // case "cloudfront":
+
+        //   break;
+        default:
+          throw new Error("Invalid Record Type Parameter");
+      }
+    } else {
+      throw Error("The number of Dns Records and Hosted Zone Names do not match.");
+    }
+  }
+}
