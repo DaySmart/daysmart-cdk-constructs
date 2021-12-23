90a3a6899b5b778811f97584ea0e071513849b99
TAYLOR GARPOW <TAYLOR.GARPOW@daysmart.com>
2021-12-23 10:35:32 -0500

    Start step functions

--- packages/cdk-s3-deployment/lib/index.ts
+++ packages/cdk-s3-deployment/lib/index.ts
@@ -6,6 +6,11 @@ import * as s3 from '@aws-cdk/aws-s3';
 import * as cloudfront from '@aws-cdk/aws-cloudfront';
 import * as iam from '@aws-cdk/aws-iam';
 import { AwsCliLayer } from '@aws-cdk/lambda-layer-awscli';
+import * as tasks from '@aws-cdk/aws-stepfunctions-tasks';
+import * as sfn from '@aws-cdk/aws-stepfunctions';
+import { StateMachine } from '@aws-cdk/aws-stepfunctions';
+import { Construct } from '@aws-cdk/core';
+import { stringify } from 'querystring';
 
 export interface CdkS3DeploymentProps {
   bucketName: string;
@@ -16,6 +21,9 @@ export interface CdkS3DeploymentProps {
   environment: string;
   snsTopicArn: string;
 }
+
+declare const submitLambda: lambda.Function;
+declare const getStatusLambda: lambda.Function;
 
 export class CdkS3Deployment extends cdk.Construct {
 
@@ -67,6 +75,53 @@ export class CdkS3Deployment extends cdk.Construct {
       ],
       resources: [`${props.snsTopicArn}`]
     }));
+
+    // New Lambda for step function
+    const sendResultLambda = new lambda.SingletonFunction(this, 'SendResultLambda', {
+      code: lambda.Code.fromInline(`
+      //Source code of lambda
+      `),
+      layers: [new AwsCliLayer(this, 'AWSCliLayer')],
+      runtime: lambda.Runtime.PYTHON_3_6,
+      handler: 'handler.handler',
+      lambdaPurpose: 'Custom::BucketDeployment',
+      timeout: cdk.Duration.minutes(15),
+      uuid: '8693BB64-9689-44B6-9AAF-B0CC9EB8756C'
+    })
+
+    // setup consts to put into step function
+    const sendResult = new tasks.LambdaInvoke(this, 'Send Result', {
+      lambdaFunction: sendResultLambda,
+      // Result is in attribute Payload
+      outputPath: '$.Payload',
+    });
+    const waitTime = new sfn.Wait(this, 'Wait X Seconds',{
+      time: sfn.WaitTime.secondsPath('$.waitSeconds'),
+    });
+    const getStatus = new tasks.LambdaInvoke(this, 'Get Job Status',{
+      lambdaFunction: getStatusLambda,
+      //Pass field name "guid" into Lambda, put Lambda result in field called "status" in response
+      inputPath: '$.guid',
+      outputPath: '$.Payload',
+    });   
+    const choice = new sfn.Choice(this, 'Did it finish validating?');
+    const successState = new sfn.Pass(this, 'SuccessState');
+    const failureState = new sfn.Pass(this, 'FailureState');
+
+    const map = new sfn.Map(this, 'Map State', {
+      maxConcurrency: 2,
+      itemsPath: sfn.JsonPath.stringAt('$.inputForMap'),
+
+    });
+    map.iterator(new sfn.Pass(this, 'Pass State'));
+
+    map.next(sendResult)
+    // Invalidate Cloud Front Distribution task,   Service integration.  
+    // Start invalidation... Get invalidation... put together
+
+    //Start State machine
+    
+
 
     new cdk.CustomResource(this, 'CustomResource', {
       serviceToken: handler.functionArn,
