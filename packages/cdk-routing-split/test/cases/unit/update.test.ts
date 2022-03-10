b28402170158fcdd90789708a49d0c53b2516541
Djordje Ivanovic <djordje.ivanovic@daysmart.com>
2022-03-10 10:03:44 +0100

    ATLAS-2777 Updated unit tests

--- packages/cdk-routing-split/test/cases/unit/update.test.ts
+++ packages/cdk-routing-split/test/cases/unit/update.test.ts
@@ -1,7 +1,5 @@
 import * as when from '../../steps/when';
 import * as given from '../../steps/given';
-import * as then from '../../steps/then';
-import { createPK } from '../../../src/shared/make-keys';
 
 describe('When an api user', () => {
     let requestBody: any;
@@ -11,44 +9,44 @@ describe('When an api user', () => {
     });
 
     const invalidKeyValues = [undefined, null, '', 'invalidKey'];
-    it.each(invalidKeyValues)('calls add with invalid key field', async (caseArg) => {
+    it.each(invalidKeyValues)('calls update with invalid key field', async (caseArg) => {
         requestBody['key'] = caseArg;
         const expectedError = {
             statusCode: 400,
             body: 'Field key is invalid. Valid values are: Subdomain, Domain, QueryStringParam, FirstPathSegment',
         };
 
-        const response = await when.we_invoke_add(requestBody);
+        const response = await when.we_invoke_update(requestBody);
 
         expect(response).toStrictEqual(expectedError);
     });
 
     const invalidValues = [undefined, null, ''];
-    it.each(invalidValues)('calls add with invalid value field', async (caseArg) => {
+    it.each(invalidValues)('calls update with invalid value field', async (caseArg) => {
         requestBody['value'] = caseArg;
         const expectedError = { statusCode: 400, body: 'Field value is required.' };
 
-        const response = await when.we_invoke_add(requestBody);
+        const response = await when.we_invoke_update(requestBody);
 
         expect(response).toStrictEqual(expectedError);
     });
 
     const invalidPriorityValues = [undefined, null, '', -1];
-    it.each(invalidPriorityValues)('calls add with invalid priority field', async (caseArg) => {
+    it.each(invalidPriorityValues)('calls update with invalid priority field', async (caseArg) => {
         requestBody['priority'] = caseArg;
         const expectedError = { statusCode: 400, body: 'Field priority must be greater than or equal to 0.' };
 
-        const response = await when.we_invoke_add(requestBody);
+        const response = await when.we_invoke_update(requestBody);
 
         expect(response).toStrictEqual(expectedError);
     });
 
     const invalidOriginValues = [undefined, null, '', 'cloud'];
-    it.each(invalidOriginValues)('calls add with invalid origin field', async (caseArg) => {
+    it.each(invalidOriginValues)('calls update with invalid origin field', async (caseArg) => {
         requestBody['origin'] = caseArg;
         const expectedError = { statusCode: 400, body: 'Field origin must be a valid url.' };
 
-        const response = await when.we_invoke_add(requestBody);
+        const response = await when.we_invoke_update(requestBody);
 
         expect(response).toStrictEqual(expectedError);
     });
