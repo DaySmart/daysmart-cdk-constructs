fbaa0cf0439e19cb3b32cb57960614054ecbd9f2
Matthew Arteaga <matthew.arteaga@daysmart.com>
2022-05-02 15:52:06 -0400

    added ingress for 443 as well

--- packages/cdk-ecs-alb/lib/index.ts
+++ packages/cdk-ecs-alb/lib/index.ts
@@ -277,9 +277,11 @@ export class CdkEcsAlb extends cdk.Construct {
 
         if (props.securityGroupIngressPort) {
             applicationLoadBalancedService.service.connections.securityGroups[0].addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(parseInt(props.securityGroupIngressPort)));  
-        } else {
-            applicationLoadBalancedService.service.connections.securityGroups[0].addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80));
         }
+
+        applicationLoadBalancedService.service.connections.securityGroups[0].addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80));
+        applicationLoadBalancedService.service.connections.securityGroups[0].addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443));
+
 
         applicationLoadBalancedService.targetGroup.configureHealthCheck({
             path: props.healthCheckPath,
