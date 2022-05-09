2c130da63faa2c7379b121a130b16c2e1b87e6d0
TAYLOR GARPOW <TAYLOR.GARPOW@daysmart.com>
2022-05-09 10:38:05 -0400

    unit test

---
+++ packages/cdk-cloudfront-origin-request-policy/test/cdk-cloudfront-origin-request-policy.test.ts
@@ -0,0 +1,31 @@
+import { Stack } from '@aws-cdk/core';
+import { CdkCloudfrontOriginRequestPolicy } from '../lib/index';
+import '@aws-cdk/assert/jest'
+import { SynthUtils } from '@aws-cdk/assert';
+
+const stack = new Stack();
+new CdkCloudfrontOriginRequestPolicy(stack, "CdkCloudfrontOriginRequestPolicy", {
+    cookieBehavior: 'all',
+    headerBehavior: 'none',
+    policyName: 'test',
+    queryStringBehavior: 'all'
+}) 
+
+const template = SynthUtils.toCloudFormation(stack)
+console.log(JSON.stringify(template, null, 2))
+
+test("Cookie Behavior", () => {
+    expect(stack).toHaveResourceLike("AWS::CloudFront::OriginRequestPolicy", {
+        OriginRequestPolicyConfig: {
+            CookiesConfig: {
+                CookieBehavior: 'all'
+            },
+            HeadersConfig: {
+                HeaderBehavior: 'none'
+            },
+            QueryStringsConfig: {
+                QueryStringBehavior: 'all'
+            }
+        }
+    })
+})
