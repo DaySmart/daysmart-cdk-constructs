32475f10dd481aac1825196ffa90205ee47c1459
TAYLOR GARPOW <TAYLOR.GARPOW@daysmart.com>
2022-05-09 13:23:35 -0400

    unit test for ecs-codedeploy-resources

---
+++ packages/cdk-ecs-codedeploy-resources/test/cdk-ecs-codedeploy-resources.test.ts
@@ -0,0 +1,33 @@
+import { Stack } from '@aws-cdk/core';
+import { CdkEcsCodedeployResources } from '../lib/index';
+import '@aws-cdk/assert/jest'
+import { SynthUtils } from '@aws-cdk/assert';
+
+const stack = new Stack();
+new CdkEcsCodedeployResources(stack, "CdkEcsCodedeployResources", {
+    appName: 'test',
+    clusterName: 'name',
+    commitHash: '1234',
+    deployBucket: 'bucket',
+    listenerArn: 'asdfg',
+    serviceName: 'taylor',
+    stage: 'test',
+    targetGroup1Name: 'testing'
+}) 
+
+const template = SynthUtils.toCloudFormation(stack)
+console.log(JSON.stringify(template, null, 2))
+
+test("Bucket Name", () => {
+    expect(stack).toHaveResource("Custom::CDKBucketDeployment", {
+        DestinationBucketName: 'bucket'
+    })
+})
+
+test("Commit Hash", () => {
+    expect(stack).toHaveResource("Custom::CDKBucketDeployment", {
+        UserMetadata: {
+            commithash: '1234'
+        }
+    })
+})
