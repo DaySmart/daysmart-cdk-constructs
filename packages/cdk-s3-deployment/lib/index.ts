6d9ea798879e13ff9d84feda6498b9ba34326ebe
TAYLOR GARPOW <TAYLOR.GARPOW@daysmart.com>
2022-06-14 11:41:34 -0400

    Remainder of Constructs that need publishing and change back to on push to main branch publish

--- packages/cdk-s3-deployment/lib/index.ts
+++ packages/cdk-s3-deployment/lib/index.ts
@@ -6,7 +6,7 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { AwsCliLayer } from 'aws-cdk-lib/lambda-layer-awscli';
-import { Construct } from 'constructs'; 
+import { Construct } from 'constructs';
 
 export interface CdkS3DeploymentProps {
   bucketName: string;
