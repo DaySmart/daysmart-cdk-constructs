06e6b06961c83cd916f42e9b4d1d9a059ad64306
TAYLOR GARPOW <TAYLOR.GARPOW@daysmart.com>
2022-06-14 09:51:04 -0400

    more testing

--- packages/cdk-api-gateway-domain-multi-region/lib/alias-target.ts
+++ packages/cdk-api-gateway-domain-multi-region/lib/alias-target.ts
@@ -8,4 +8,4 @@ export function getAliasTarget(props: GetAliasTargetProps) {
     return  props.dynamicEnv ? `${props.dynamicEnv}-api.${props.baseEnv}.${props.project}.${props.companyDomainName}` :
         (props.baseEnv == "prod") ? `api.${props.project}.${props.companyDomainName}` : 
             `api.${props.baseEnv}.${props.project}.${props.companyDomainName}`;
-}
+} 
