0dbddf71281979315fad2e959dc129f9cfeae546
TAYLOR GARPOW <TAYLOR.GARPOW@daysmart.com>
2022-06-17 09:03:01 -0400

    update index to export all, version bump

--- packages/cdk-global-endpoints/lib/index.ts
+++ packages/cdk-global-endpoints/lib/index.ts
@@ -1,2 +1,4 @@
 export { ApiGatewayDomainMultiRegion, ApiGatewayDomainMultiRegionProps } from './api-gateway-multi-region-domain';
 export { ApiGatewayDomainRoute53Alias, ApiGatewayDomainRoute53AliasProps} from './api-gateway-route53-alias';
+export { CustomDomain, CustomDomainProps } from './api-domain-name';
+export { CdkApiGatewayDomain, CdkApiGatewayDomainProps } from './api-gateway-domain'
