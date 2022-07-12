fd96bf9193da0c5ccc62c12779c5d45ed74fcb51
TAYLOR GARPOW <TAYLOR.GARPOW@daysmart.com>
2022-07-12 13:23:16 -0400

    combine unit test, add point recovery to also

--- packages/cdk-dynamodbtable/test/cdk-dynamodbtable-v2.test.ts
+++ packages/cdk-dynamodbtable/test/cdk-dynamodbtable-v2.test.ts
@@ -14,9 +14,34 @@ test('App Cloudfront', () => {
     });
 
     const template = Template.fromStack(stack);
-    console.log(JSON.stringify(template, null, 2));
 
     template.hasResourceProperties('AWS::DynamoDB::Table', {
         TableName: 'test'
     });
+})
+
+test('Point In Time Recovery', () => {
+    const stack = new cdk.Stack(undefined, 'stack', {
+        env: {
+            account: '12345',
+            region: 'us-east-1'
+        }
+    });
+    new CdkDynamodbtable(stack, 'AppCloudfront', {
+        tableName: 'testing',
+        pointInTimeRecovery: true
+    });
+
+    const template = Template.fromStack(stack);
+    console.log(JSON.stringify(template, null, 2));
+
+    template.hasResourceProperties('AWS::DynamoDB::Table', {
+        TableName: 'testing'
+    });
+
+    template.hasResourceProperties('AWS::DynamoDB::Table', {
+        PointInTimeRecoverySpecification: {
+            PointInTimeRecoveryEnabled: true
+        }
+    })
 })
