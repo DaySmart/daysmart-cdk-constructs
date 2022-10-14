4d256ed6a9c911f156488d4d637a62a898ad92fe
TAYLOR GARPOW <TAYLOR.GARPOW@daysmart.com>
2022-10-14 11:15:29 -0400

    memcache cluster

--- packages/cdk-cache-cluster/test/cache-cluster.test.ts
+++ packages/cdk-cache-cluster/test/cache-cluster.test.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template } from "aws-cdk-lib/assertions";
-import { CacheCluster } from '../lib/cacheCluster';
+import { CacheCluster } from '../lib/index';
 
 test('Cache', () => {
     const stack = new cdk.Stack(undefined, 'stack', {
@@ -12,20 +12,22 @@ test('Cache', () => {
     new CacheCluster(stack, 'CacheCluster', {
         cacheNodeType: 'cache.m6g.large',
         engine: 'memcached',
-        numCacheNodes: 1,
+        numCacheNodes: '1',
         clusterName: 'rate-limit',
-        cacheSubnetGroupName: 'accept-testing',
         port: 11211,
+        vpcId: 'vpc-12345abcde',
+        cacheSubnetGroupName: ['subnetgroup'],
+        vpcSecurityGroupIds: ['securitygroup']
 
     });
 
     const template = Template.fromStack(stack);
+    console.log(JSON.stringify(template, null, 2));
 
     template.hasResourceProperties('AWS::ElastiCache::CacheCluster', {
         CacheNodeType: 'cache.m6g.large',
         Engine: 'memcached',
         NumCacheNodes: 1,
-        CacheSubnetGroupName: 'accept-testing',
         ClusterName: 'rate-limit',
         Port: 11211,
     });
